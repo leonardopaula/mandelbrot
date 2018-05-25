71d22ec22b57aba6ece89af233d4d965ba931bef
gSchroeder <guto_schroeder@hotmail.com>
2018-05-24 22:06:26 -0300

    Linux +  Mac + Windows

--- mandelbrot.c
+++ mandelbrot.c
@@ -2,8 +2,16 @@
 #include <stdlib.h>
 #include <math.h>
 #include <GL/glut.h>
+#ifdef __APPLE__
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
+#else
+#ifdef _WIN32
+  #include <windows.h>
+#endif
+#include <GL/gl.h>
+#include <GL/glu.h>
+#endif
 
 //gcc -o mandelbrot  mandelbrot.c -L/System/Library/Frameworks -framework GLUT -framework OpenGL
 
